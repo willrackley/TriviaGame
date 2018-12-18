efe36d19597dab122fe07f36798e9812f520b5f1
williamrackley <willrackley65@gmail.com>
2018-12-17 20:47:22 -0500

    finished the logic of the game, need to add css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,110 +1,46 @@
-/* ------Psuedo Code------
--   trivia game that displays one question at a time and has a timer for that particular question
--   after the player guesses then they need to be shown if they got it right or wrong
--   after the user has guessed or the time has run out, the next question needs to come up automatically
--   if the user guess wrong then they need to be shown the correct answer
--   the final screen needs to show how many they guessed right or wrong and needs to give them the option to start over without             reloading the page
--   need to create html for the app that has a title, a div for diplaying the question and possibly use that same div to display if the     user won or lost
-*/ 
-
 $(document).ready(function() {
 
 var correctAnswer = 0;
 var wrongAnswer = 0;
 var timeRemaining = 30;
-intervalId = setInterval(timerCountdown, 1000);
-var questionsArray = [{question: "A canine's sense of smell is 100,000x stronger than a human's", answer: "True"}/*, { question: "Three dogs survived the sinking of the Titanic", answer: "True"},{question: "Dalmations are born with spots", answer: "False"}, {question: "Dogs’eyes contain a special membrane, which allows them to see in the dark.", answer: "True"}, {question: "Dogs have only two eyelids, just like us", answer: "False"}, {question: "Dogs are colorblind.", answer: "False"}, {question: "Dogs only sweat through the pads of their feet", answer: "True"}, {question: "Dogs that have not been spayed or neutered live longer", answer: "False"}, {question: "Every single U.S. President has owned a dog", answer: "False"}, {question: "The shape of a dog’s face suggests its longevity: A long face means a longer life.", answer: "True"}*/, {question: "Game Over"}];
+var indexIncrement = 0;
+var intervalId;
+var clockRunning = false;
+var questionsArray = [{question: "A canine's sense of smell is 100,000x stronger than a human's", answer: "True"}, { question: "Three dogs survived the sinking of the Titanic", answer: "True"}/*,{question: "Dalmations are born with spots", answer: "False"}, {question: "Dogs’eyes contain a special membrane, which allows them to see in the dark.", answer: "True"}, {question: "Dogs have only two eyelids, just like us", answer: "False"}, {question: "Dogs are colorblind.", answer: "False"}, {question: "Dogs only sweat through the pads of their feet", answer: "True"}, {question: "Dogs that have not been spayed or neutered live longer", answer: "False"}, {question: "Every single U.S. President has owned a dog", answer: "False"}, {question: "The shape of a dog’s face suggests its longevity: A long face means a longer life.", answer: "True"}*/, {question: "Game Over"}];
 var randGif;
 
 function displayQuestion(){
+    
+    intervalId = setInterval(timerCountdown, 1000);
+    console.log("number of wins " + correctAnswer);
+    console.log("number of losses " + wrongAnswer);
     randGif = Math.floor(Math.random() * 20);
     timerCountdown();
-    for(var i = 0; i < questionsArray.length; i++){
-        console.log(questionsArray[i].question);
-        var questionDisplay = $("#question").text(questionsArray[i].question);
-    }
-    $("#question").text(questionsArray[0].question);
+    $("#question").text(questionsArray[indexIncrement].question);
+    
 }
 
 
 function nextQuestion(){
     randGif = Math.floor(Math.random() * 20);
-    intervalId = setInterval(timerCountdown, 1000);
     timeRemaining = 30;
-    //timerCountdown();
+    timerCountdown();
     $("#gifPic").html("");
     $("#answer").text("");
+    indexIncrement++;
     displayQuestion();
-    
-    if(questionsArray[0].question === "Game Over"){
+
+    if(questionsArray[indexIncrement].question === "Game Over") {
         endGame();
     }
-}
    
-
-function timerCountdown() {
-    if (timeRemaining == 0 && questionsArray.length > 0) {
-        $("#answer").text("Time's Up!");
-        $("#timer").text("0 Seconds Remaining");
-      clearTimeout(intervalId);
-
-      $.ajax({
-        url: "http://api.giphy.com/v1/gifs/search?q=sad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
-        method: "GET"
-      }).then(function(response) {
-          
-          $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
-          
-      });
-      wrongAnswer++;
-      questionsArray.splice(0,1);
-      setTimeout(nextQuestion, 5000);
-    } else {
-        $("#timer").text(timeRemaining + " Seconds Remaining");
-      timeRemaining--;
-    }
-  }
-
-  function endGame(){
-    timeRemaining = 0;
-    clearTimeout(intervalId);
-    clearTimeout(setTimeout);
-    $("#question").html("Correct Answers: " + correctAnswer + "\n" + "Incorrect Answers: " + wrongAnswer).wrap('<pre />');
-    $("#timer").text("");
-    $("#trueButton").hide();
-    $("#falseButton").hide();
-    $("#tryAgainButton").show();
-    $("#tryAgainButton").text("Try Again");
-    $.ajax({
-        url: "http://api.giphy.com/v1/gifs/search?q=funny+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
-        method: "GET"
-      }).then(function(response) { 
-          $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
-      });
-  }
-
-  function resetGame(){
-    var questionsArray = [{question: "A canine's sense of smell is 100,000x stronger than a human's", answer: "True"}, { question: "Three dogs survived the sinking of the Titanic", answer: "True"},{question: "Dalmations are born with spots", answer: "False"}, {question: "Dogs’eyes contain a special membrane, which allows them to see in the dark.", answer: "True"}, {question: "Dogs have only two eyelids, just like us", answer: "False"}, {question: "Dogs are colorblind.", answer: "False"}, {question: "Dogs only sweat through the pads of their feet", answer: "True"}, {question: "Dogs that have not been spayed or neutered live longer", answer: "False"}, {question: "Every single U.S. President has owned a dog", answer: "False"}, {question: "The shape of a dog’s face suggests its longevity: A long face means a longer life.", answer: "True"}, {question: "Game Over"}];
-    intervalId = setInterval(timerCountdown, 1000);
-    timeRemaining = 30;
-    randGif = Math.floor(Math.random() * 20);
-    var correctAnswer = 0;
-    var wrongAnswer = 0;
-    $("#gifPic").html("")
-    $("#tryAgainButton").hide();
-    $("#trueButton").show();
-    $("#falseButton").show();
-    timerCountdown();
-
-    for(var i = 0; i < questionsArray.length; i++){
-        console.log(questionsArray[i].question);
-        var questionDisplay = $("#question").text(questionsArray[i].question);
-    }
-    $("#question").text(questionsArray[0].question);
-
     $("#trueButton").one("click", function(){
+        $(this).data('clicked', true);
+        if($("#trueButton").data('clicked')){
+            $("#falseButton").off("click");
+        }
         
-        if(questionsArray[0].answer === "True" ){
+        if(questionsArray[indexIncrement].answer === "True" ){
         $("#answer").text("Correct!");
         correctAnswer++;
         $.ajax({
@@ -113,81 +49,32 @@ function timerCountdown() {
           }).then(function(response) {
               $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");  
           });
-        questionsArray.splice(0,1);
+        
         clearInterval(intervalId);
         setTimeout(nextQuestion, 5000);
-        console.log("number of wins " + correctAnswer);
-        console.log("number of losses " + wrongAnswer);
+       
         
         } else {
             $("#answer").text("Wrong!");
             wrongAnswer++;
-             $.ajax({
-                 url: "http://api.giphy.com/v1/gifs/search?q=sad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
-                 method: "GET"
-               }).then(function(response) {
-                   
-                   $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
-                   
-               });
-               
-            questionsArray.splice(0,1);
-             clearInterval(intervalId);
-             setTimeout(nextQuestion, 5000);
-             console.log("number of wins " + correctAnswer);
-             console.log("number of losses " + wrongAnswer);
-             
+            $.ajax({
+                url: "http://api.giphy.com/v1/gifs/search?q=sad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+                method: "GET"
+            }).then(function(response) {  
+                $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
+            }); 
+            
+            clearInterval(intervalId);
+            setTimeout(nextQuestion, 5000);
         }
-
-    } );
-
-}
-
-
-displayQuestion();
-
-
-    $("#trueButton").one("click", function(){
-        
-        if(questionsArray[0].answer === "True" ){
-        $("#answer").text("Correct!");
-        correctAnswer++;
-        $.ajax({
-            url: "http://api.giphy.com/v1/gifs/search?q=happy+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
-            method: "GET"
-          }).then(function(response) {
-              $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");  
-          });
-        questionsArray.splice(0,1);
-        clearInterval(intervalId);
-        setTimeout(nextQuestion, 5000);
-        console.log("number of wins " + correctAnswer);
-        console.log("number of losses " + wrongAnswer);
-        
-        } else {
-            $("#answer").text("Wrong!");
-            wrongAnswer++;
-             $.ajax({
-                 url: "http://api.giphy.com/v1/gifs/search?q=sad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
-                 method: "GET"
-               }).then(function(response) {
-                   
-                   $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
-                   
-               });
-               
-            questionsArray.splice(0,1);
-             clearInterval(intervalId);
-             setTimeout(nextQuestion, 5000);
-             console.log("number of wins " + correctAnswer);
-             console.log("number of losses " + wrongAnswer);
-             
+    
+    });
+    $("#falseButton").one("click", function(){
+       $(this).data('clicked', true);
+        if($("#falseButton").data('clicked')){
+            $("#trueButton").off("click");
         }
-
-    } );
-
-    $("#falseButton").on("click", function(){
-        if(questionsArray[0].answer === "False" ){
+        if(questionsArray[indexIncrement].answer === "False" ){
         $("#answer").text("Correct");
         
         correctAnswer++;
@@ -198,13 +85,8 @@ displayQuestion();
               $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
           });
           
-        questionsArray.splice(0,1);
         clearInterval(intervalId);
         setTimeout(nextQuestion, 5000);
-        
-        
-        console.log("number of wins " + correctAnswer);
-        console.log("number of losses " + wrongAnswer);
         
         } else {
             $("#answer").text("Wrong!");
@@ -220,17 +102,156 @@ displayQuestion();
                 });
               clearInterval(intervalId);
               setTimeout(nextQuestion, 5000);
-             
-              questionsArray.splice(0,1);
-              console.log("number of wins " + correctAnswer);
-              console.log("number of losses " + wrongAnswer);
-              
+                 
         }
         
     });
+}
+   
+   
 
-    $("#tryAgainButton").on("click", function(){
-        resetGame();
-    });
+function timerCountdown() {
+    
+    if (timeRemaining == 0) {
+        $("#answer").text("Time's Up!");
+        $("#timer").text("0 Seconds Remaining");
+      clearTimeout(intervalId);
+      clockRunning = false;
+      $.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?q=sad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+        method: "GET"
+      }).then(function(response) {
+          
+          $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
+          
+      });
+
+      wrongAnswer++;
+      setTimeout(nextQuestion, 5000);
+    } else {
+        $("#timer").text(timeRemaining + " Seconds Remaining");
+      timeRemaining--;
+      clockRunning = true;
+    }
+  }
+
+function endGame(){
+    clearTimeout(intervalId);
+    clearTimeout(setTimeout);
+    $("#question").html("Correct Answers: " + correctAnswer + "\n" + "Incorrect Answers: " + wrongAnswer).wrap('<pre />');
+    $("#timer").text("");
+    $("#trueButton").hide();
+    $("#falseButton").hide();
+    $("#tryAgainButton").show();
+    $("#tryAgainButton").text("Try Again");
+    $.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?q=funny+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+        method: "GET"
+      }).then(function(response) { 
+          $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
+      });
+      
+  }
+
+  function resetGame(){
+    
+    /*questionsArray = [{question: "A canine's sense of smell is 100,000x stronger than a human's", answer: "True"}, { question: "Three dogs survived the sinking of the Titanic", answer: "True"},{question: "Dalmations are born with spots", answer: "False"}, {question: "Dogs’eyes contain a special membrane, which allows them to see in the dark.", answer: "True"}, {question: "Dogs have only two eyelids, just like us", answer: "False"}, {question: "Dogs are colorblind.", answer: "False"}, {question: "Dogs only sweat through the pads of their feet", answer: "True"}, {question: "Dogs that have not been spayed or neutered live longer", answer: "False"}, {question: "Every single U.S. President has owned a dog", answer: "False"}, {question: "The shape of a dog’s face suggests its longevity: A long face means a longer life.", answer: "True"}, {question: "Game Over"}];*/
+    
+    timerCountdown();
+   
+    timeRemaining = 30;
+    indexIncrement = 0;
+    randGif = Math.floor(Math.random() * 20);
+    correctAnswer = 0;
+    wrongAnswer = 0;
+    $("#gifPic").html("")
+    $("#tryAgainButton").hide();
+    $("#trueButton").show();
+    $("#falseButton").show();
+    displayQuestion();
+
+    
+}
+
+
+displayQuestion();
+
+
+$("#trueButton").one("click", function(){
+    $(this).data('clicked', true);
+    if($("#trueButton").data('clicked')){
+        $("#falseButton").off("click");
+    }
+    
+    if(questionsArray[indexIncrement].answer === "True" ){
+    $("#answer").text("Correct!");
+    correctAnswer++;
+    $.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?q=happy+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+        method: "GET"
+      }).then(function(response) {
+          $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");  
+      });
+    
+    clearInterval(intervalId);
+    setTimeout(nextQuestion, 5000);
+   
+    
+    } else {
+        $("#answer").text("Wrong!");
+        wrongAnswer++;
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=sad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+            method: "GET"
+        }).then(function(response) {  
+            $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
+        }); 
+        
+        clearInterval(intervalId);
+        setTimeout(nextQuestion, 5000);
+    }
+
+});
+$("#falseButton").one("click", function(){
+   $(this).data('clicked', true);
+    if($("#falseButton").data('clicked')){
+        $("#trueButton").off("click");
+    }
+    if(questionsArray[indexIncrement].answer === "False" ){
+    $("#answer").text("Correct");
+    
+    correctAnswer++;
+    $.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?q=sad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+        method: "GET"
+      }).then(function(response) { 
+          $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
+      });
+      
+    clearInterval(intervalId);
+    setTimeout(nextQuestion, 5000);
+    
+    } else {
+        $("#answer").text("Wrong!");
+        
+        wrongAnswer++;
+          $.ajax({
+              url: "http://api.giphy.com/v1/gifs/search?q=happy+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+              method: "GET"
+            }).then(function(response) {
+                
+                $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
+                
+            });
+          clearInterval(intervalId);
+          setTimeout(nextQuestion, 5000);
+    }
+    
+});
+$("#tryAgainButton").on("click", function(){
+    resetGame();
+});
+
+
 
 });
