6f3c849b9084f7e303faab609cb58b85f2368268
williamrackley <willrackley65@gmail.com>
2018-12-18 20:24:40 -0500

    added a start screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,22 +9,37 @@ var clockRunning = false;
 var questionsArray = [{question: "A canine's sense of smell is 100,000x stronger than a human's", answer: "True"}, { question: "Three dogs survived the sinking of the Titanic", answer: "True"}/*,{question: "Dalmations are born with spots", answer: "False"}, {question: "Dogs’eyes contain a special membrane, which allows them to see in the dark.", answer: "True"}, {question: "Dogs have only two eyelids, just like us", answer: "False"}, {question: "Dogs are colorblind.", answer: "False"}, {question: "Dogs only sweat through the pads of their feet", answer: "True"}, {question: "Dogs that have not been spayed or neutered live longer", answer: "False"}, {question: "Every single U.S. President has owned a dog", answer: "False"}, {question: "The shape of a dog’s face suggests its longevity: A long face means a longer life.", answer: "True"}*/, {question: "Game Over"}];
 var randGif;
 
-function displayQuestion(){
+function startGame(){
+    clearTimeout(intervalId);
+    $("#timer").hide();
+    $("#trueButton").hide();
+    $("#falseButton").hide();
+    $("#question").text("There are 10 true or false questions that all have to do with man's best friend. You will have 30 seconds to answer each question. Good Luck!")
     
+}
+function displayQuestion(){
+    $("#startButton").hide()
+    $("#timer").show();
+    $("#trueButton").show();
+    $("#falseButton").show();
+    clockRunning = true;
     intervalId = setInterval(timerCountdown, 1000);
     console.log("number of wins " + correctAnswer);
     console.log("number of losses " + wrongAnswer);
     randGif = Math.floor(Math.random() * 20);
-    timerCountdown();
+    //timerCountdown();
     $("#question").text(questionsArray[indexIncrement].question);
     
 }
 
 
 function nextQuestion(){
+    $("#startButton").hide()
     randGif = Math.floor(Math.random() * 20);
     timeRemaining = 30;
     timerCountdown();
+    $("#trueButton").show();
+    $("#falseButton").show();
     $("#gifPic").html("");
     $("#answer").text("");
     indexIncrement++;
@@ -102,23 +117,27 @@ function nextQuestion(){
                 });
               clearInterval(intervalId);
               setTimeout(nextQuestion, 5000);
-                 
         }
-        
     });
+    //$("#tryAgainButton").on("click", function(){
+        //resetGame();
+   // });
 }
    
    
 
 function timerCountdown() {
     
-    if (timeRemaining == 0) {
+    if (timeRemaining === 0) {
         $("#answer").text("Time's Up!");
         $("#timer").text("0 Seconds Remaining");
-      clearTimeout(intervalId);
-      clockRunning = false;
+        $("#trueButton").hide();
+        $("#falseButton").hide();
+        $("#startButton").hide()
+        clearTimeout(intervalId);
+        clockRunning = false;
       $.ajax({
-        url: "http://api.giphy.com/v1/gifs/search?q=mad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+        url: "http://api.giphy.com/v1/gifs/search?q=sad+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
         method: "GET"
       }).then(function(response) {
           
@@ -142,10 +161,11 @@ function endGame(){
     $("#timer").text("");
     $("#trueButton").hide();
     $("#falseButton").hide();
+    $("#startButton").hide()
     $("#tryAgainButton").show();
     $("#tryAgainButton").text("Try Again");
     $.ajax({
-        url: "http://api.giphy.com/v1/gifs/search?q=funny+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
+        url: "http://api.giphy.com/v1/gifs/search?q=happy+dog&api_key=17HlEsY0GKfVxvXvmi1HZw2RI94pGhFc&limit=20",
         method: "GET"
       }).then(function(response) { 
           $("#gifPic").html("<img src=" + response.data[randGif].images.original.url + ">");
@@ -154,17 +174,15 @@ function endGame(){
   }
 
   function resetGame(){
-    
-    /*questionsArray = [{question: "A canine's sense of smell is 100,000x stronger than a human's", answer: "True"}, { question: "Three dogs survived the sinking of the Titanic", answer: "True"},{question: "Dalmations are born with spots", answer: "False"}, {question: "Dogs’eyes contain a special membrane, which allows them to see in the dark.", answer: "True"}, {question: "Dogs have only two eyelids, just like us", answer: "False"}, {question: "Dogs are colorblind.", answer: "False"}, {question: "Dogs only sweat through the pads of their feet", answer: "True"}, {question: "Dogs that have not been spayed or neutered live longer", answer: "False"}, {question: "Every single U.S. President has owned a dog", answer: "False"}, {question: "The shape of a dog’s face suggests its longevity: A long face means a longer life.", answer: "True"}, {question: "Game Over"}];*/
-    
-    timerCountdown();
-   
+    //timerCountdown();
+    clearTimeout(intervalId);
     timeRemaining = 30;
     indexIncrement = 0;
     randGif = Math.floor(Math.random() * 20);
     correctAnswer = 0;
     wrongAnswer = 0;
     $("#gifPic").html("")
+    $("#startButton").hide()
     $("#tryAgainButton").hide();
     $("#trueButton").show();
     $("#falseButton").show();
@@ -173,9 +191,11 @@ function endGame(){
     
 }
 
-
-displayQuestion();
-
+timerCountdown();
+startGame();
+$("#startButton").one("click", function(){
+    displayQuestion();
+});
 
 $("#trueButton").one("click", function(){
     $(this).data('clicked', true);
